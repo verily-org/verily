19cedef98be3da49604bedecc914091ed0d79d93
Alexander Greenland <ajrg1g13@soton.ac.uk>
2014-02-23 14:30:01 +0000

    mostly work on Post create/edit view with support for date occurred; validation generalised further to be included on client and server side

--- helpers/swig.js
+++ helpers/swig.js
@@ -6,11 +6,39 @@ module.exports = function(swig) {
         utils.date.relativeTime(input);
     });
     
+    swig.setFilter('getMaxTitleLength', function() {
+        return common.getMaxTitleLength();
+    });
+    
     swig.setFilter('getYears', function() {
         return common.getYears();
     });
     
     swig.setFilter('getAllFromRange', function(start, end, padding) {
         return common.getAllFromRange(start, end, padding);
+    });
+    
+    swig.setFilter('getDayDefault', function() {
+        return common.dayDefault();
+    });
+    
+    swig.setFilter('getMonthDefault', function() {
+        return common.monthDefault();
+    });
+    
+    swig.setFilter('getYearDefault', function() {
+        return common.yearDefault();
+    });
+    
+    swig.setFilter('getHourDefault', function() {
+        return common.hourDefault();
+    });
+    
+    swig.setFilter('getMinuteDefault', function() {
+        return common.minuteDefault();
+    });
+    
+    swig.setFilter('getSecondDefault', function() {
+        return common.secondDefault();
     });
 };
--- static/js/common.js
+++ static/js/common.js
@@ -49,19 +49,112 @@ var getAllFromRange = exports.getAllFromRange = common.getAllFromRange = functio
     if (format === 'padding') {
         pad = 10;
     }
-        
+    
     // Zero padding.
     for (var i = start; i < pad && i <= end; i++) {
-        returner.push('0' + i.toString());
+        var item = i.toString();
+        if (format === 'padding') {
+            returner.push('0' + item);
+        } else {
+            returner.push(item);
+        }
     }
-    
-    // We have done padding, so start iterating at value of pad.
-    if (pad !== Number.MAX_VALUE) {    
+        
+    if (format === 'padding') {
+        // We have done padding, so start iterating at value of pad.
         for (var i = pad; i <= end; i++) {
             returner.push(i.toString());
         }
     }
     
     return returner;
-    
 };
+
+// maxTitleLength defined in generic/title-chars-left.html
+// @return boolean: true for valid
+var validateFormTitle = exports.validateFormTitle = common.validateFormTitle = function(value, elemsParent, elemsIsArray, callback) {
+    var error = null;
+    
+    if (value.length === 0) {
+        error = 'need one!';
+    } else if (value.length > common.maxTitleLength) {
+        error = 'too long!';
+    }
+    
+    callback(error, value);
+};
+
+// Used at client and server side.
+var validateDateTimeOccurred = exports.validateDateTimeOccurred = common.validateDateTimeOccurred = function(value, elemsParent, elemsIsArray, callback) {
+    var error = null;
+    var date = new Date();
+    
+    // We are dealing with an array of values (dd, mm, yyyy, h, m, s)
+    var day = value[0];
+    var month = value[1];
+    var year = value[2];
+    var hour = value[3];
+    var minute = value[4];
+    var second = value[5];
+            
+    // If they didn't select a date at all, that's fine -- the date is optional.
+    if (day !== common.dayDefault() ||
+        month !== common.monthDefault() ||
+        year !== common.yearDefault() ||
+        hour !== common.hourDefault() ||
+        minute !== common.minuteDefault() ||
+        second !== common.secondDefault()) {
+            // There is at least one field not set to its default (placeholder), so:
+            
+            day = parseInt(day);
+            month = parseInt(month);
+            year = parseInt(year);
+            hour = parseInt(hour);
+            minute = parseInt(minute);
+            second = parseInt(second);
+            
+            // Try to construct a date object out of these fields' values.
+            // var date = new Date(year, month, day, hour, minute, second);
+            date.setUTCFullYear(year);
+            date.setUTCMonth(month - 1);
+            date.setUTCDate(day);
+            date.setUTCHours(hour);
+            date.setUTCMinutes(minute);
+            date.setUTCSeconds(second);
+                    
+            if (isNaN(date.getTime()) || date.getUTCDate() !== day || date.getUTCMonth() + 1 !== month || date.getUTCFullYear() !== year || date.getUTCHours() !== hour || date.getUTCMinutes() !== minute || date.getUTCSeconds() !== second) {
+                // Invalid date.
+                error = 'not valid!';
+            }
+        }
+    
+    callback(error, date);
+};
+
+// TODO may want this to be <140 to allow linking of the resource.
+var maxTitleLength = exports.maxTitleLength = common.maxTitleLength = 140;
+
+var dayDefault = exports.dayDefault = common.dayDefault = function() {
+    return "Day";
+}
+
+var monthDefault = exports.monthDefault = common.monthDefault = function() {
+    return "Month";
+}
+
+var yearDefault = exports.yearDefault = common.yearDefault = function() {
+    return "Year";
+}
+
+var hourDefault = exports.hourDefault = common.hourDefault = function() {
+    return "Hours";
+}
+
+var minuteDefault = exports.minuteDefault = common.minuteDefault = function() {
+    return "Minutes";
+}
+
+var secondDefault = exports.secondDefault = common.secondDefault = function() {
+    return "Seconds";
+}
+
