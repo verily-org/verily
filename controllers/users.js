605b276b34189a3d61043a81781b6aaf5b46f5c1
Dimitrios Papamilios <dp5g13@ecs.soton.ac.uk>
2014-03-11 17:59:08 +0000

    When a user registers he is logged in automatically.

--- controllers/users.js
+++ controllers/users.js
@@ -17,7 +17,7 @@ exports.profile = function (req, res) {
 };
 //Posts a new user
 exports.register = passport.authenticate('local-register', {
-    successRedirect : '/login', // redirect to the secure profile section
+    successRedirect : '/question', // redirect to the secure profile section
     failureRedirect : '/register', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
 });
