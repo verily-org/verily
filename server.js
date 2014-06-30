666adcd87708647e9e14a7a08a376b75dd8e34c4
Alex Greenland <a.greenland.6400@gmail.com>
2014-06-30 14:34:49 +0100

    model update and persistent sessions through session store mapping to node-orm2

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ module.exports = function (suppressLogs) {
         enums = require('./enums'),
         router = require('./routing/router'),
         log = require('./log'),
+        ORMSessionStore = require('./orm-session-store')(express),
         controllers = {},
         heroku,
+        syncedModels,
         db_url;
 
     //initial log functions.
@@ -62,7 +64,7 @@ module.exports = function (suppressLogs) {
 
     //prevent big package and return 413 error
     // app.use(connect.limit('5kb'));
-    app.use(connect.limit('5mb'));
+    app.use(connect.limit('10mb'));
 
     // Overwrite demo.sh at the start of execution because it is appended to.
     //if (enums.document) {
@@ -116,6 +118,10 @@ module.exports = function (suppressLogs) {
                 models.User = db.models.user;
                 models.Local = db.models.local;
                 models.Facebook = db.models.facebook;
+                models.Impression = db.models.impression;
+                models.Referral = db.models.referral;
+                models.Session = db.models.session;
+                models.SocialEvent = db.models.socialEvent;
 
                 models.Local.sync(function () {console.log("Local synced")});
                 models.QuestionComment.sync(function () {console.log("QuestionComment synced")});
@@ -127,43 +133,277 @@ module.exports = function (suppressLogs) {
                 models.Answer.sync(function () {console.log("Answer synced")});
                 models.Rating.sync(function () {console.log("Rating synced")});
                 models.Facebook.sync(function () {console.log("Facebook synced")});
+                models.Impression.sync(function () {console.log("Impression synced")});
+                models.Referral.sync(function () {console.log("Referral synced")});
+                models.Session.sync(function () {console.log("Session synced")});
+                models.SocialEvent.sync(function () {console.log("SocialEvent synced")});
 
                 // Post is the base class.
                 // Questions, answers and comments are types of Post.
 
                 db.sync(function () {
+                    syncedModels = models;
                     emitter.emit('model-synced');
                     if (!suppressLogs) {
                         console.logger.info("Model synchronised");
                     }
+                                        
                 });
             });
             next();
         }
     }));
+    
+    // /r/<referral path>
+    var referralPathFormat  = /\/r\/([A-Za-z0-9_-]+)/;
+    
+    // Association setting with fallback.
+    // methodName e.g. setUser used for some, setAuthor used otherwise.
+    function setUserOfInstance(createdInstance, user, callback) {
+        if (!user) {
+            callback();
+        } else {
+            createdInstance.setUser(user, function (err) {
+                if (err) {
+                    console.log(err);
+                }
+                callback();
+            }); 
+        }
+    }
+    
+    // Express middleware for analytics
+    var analytics = function(req, res, next) {
 
-    app.use(express.cookieParser());
-    app.use(express.bodyParser({
-        uploadDir: __dirname + '/static/images/submissions-pre'
-    }));
-    app.use(express.session({ secret: 'cat' }, {maxAge: new Date(Date.now() + 3600000)}));
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(roles.user.middleware());
-    app.use(flash());
-    app.use(app.router);
+        var impression = {
+            path: req.path,
+            requestMethod: req.method,
+            date: new Date(Date.now()),
+            ip: req.ip,
+            userAgent: req.headers['user-agent'],
+            httpRefererUrl: req.headers['referer']
+        }
+                
+        function setReferralOfImpression(createdImpression, refCode, callback) {
+            if (!refCode) {
+                callback();
+            } else {
+                req.models.Referral.find({
+                    refCode: refCode
+                }, 1, function(err, referrals) {
+                    if (err) {
+                        console.log(err);
+                        callback();
+                    } else if (referrals) {
+                        var referral = referrals[0];
+                        console.log('referral in set');
+                        console.log(referral)
+                        createdImpression.setReferral(referral, function (err) {
+                           if (err) {
+                               console.log(err);
+                           }
+                           callback();
+                   
+                       });
+                    }
+ 
+               });
+            }
+        }
+        
+        // req.user is only defined if user is logged in.
+        var user = req.user;
+        
+        // If this is a generated referral path being requested, redirect to the path intended.
+        var refPath = referralPathFormat.exec(impression.path);
+        
+        // If the user arrives via a referral code with a GET (not a registration of a refcode via POST/PUT).
+        // We do not want to create an impression for the PUT to a /r/ path because the referral won't exist yet.
+        if (refPath && req.method.toLowerCase() === 'get') {
+            var refCode = refPath[1];
+            // Referral short link
+            req.models.Referral.find({
+                refCode: refCode
+            }, 1, function(err, referrals) {
+                var referral = referrals[0];
+                console.log('get referral');
+                console.log(referral);
+                if (!err && referral) {
+                    // Add to refcodes in session.
+                    if (req.session.refcodes && Array.isArray(req.session.refcodes)) {
+                        // Push to array.
+                        req.session.refcodes.push(refCode);
+                    } else {
+                        // Create array.
+                        req.session.refcodes = [refCode];
+                    }
+                    console.log('---- REFCODES -----');
+                    console.log(req.session.refcodes);
+                    res.redirect(referral.destinationPath);
+                    res.end();
+                } else {
+                    res.redirect('/');
+                    res.end();
+                }
+            });
+        } else if (req.path !== '/favicon.ico') {
+            // This is an impression.
+            // Do not count favicon.ico requests.
+            // Record the metadata of the impression.
+            
+            console.log('==================== IMPRESSION ' + req.path +  ' ====================');
+            
+            var refCode = req.query.r;
+            
+            req.models.Impression.create([impression], function (err, items) {
+                if (err) {
+                    console.log(err);
+                }
+                                
+                var createdImpression = items[0];
+                
+                // Sets user of impression, if user is logged in.
+                setUserOfInstance(createdImpression, user, function() {
 
-//    app.listen();
-//
-//    if (!suppressLogs) {
-//        console.logger.info('Server started on ' + enums.options.hostname + ':' + enums.options.port);
-//    }
-//
-//    // Configure the routes.
-    router(app, controllers);
-    http.createServer(app).listen(app.get('port'), function(){
-        console.log('Express server listening on port ' + app.get('port'));
+                    // Sets referral of impression, if there is a ref code (if user was referred).
+                    setReferralOfImpression(createdImpression, refCode, function() {
+                        
+                       createdImpression.save(function (err) {
+                          if (err) {
+                              console.log(err);
+                          }
+
+                         next();
+                       });
+                    });
+                });
+            });
+                        
+        }
+
+    }
+    
+    
+    // Start everything up once the models have synced.
+    emitter.on('model-synced', function() {
+        startUp();
     });
+    
+    // Start everything up.
+    function startUp() {
+        
+        app.use(express.cookieParser());
+        app.use(express.bodyParser({
+            uploadDir: __dirname + '/static/images/submissions-pre'
+        }));
+                
+        app.use(express.session({
+            secret: 'd9WvgPUdReT8D3dH50FUXuwkpMOcAxA1Nll8sLG9j1s',
+            store: new ORMSessionStore(syncedModels)
+
+        }, {maxAge: new Date(Date.now() + 3600000)}));
+        app.use(passport.initialize());
+        app.use(passport.session());
+
+        app.use(roles.user.middleware());
+        app.use(flash());
+        app.use(analytics);
+        
+        app.use(app.router);
+
+
+    //    app.listen();
+    //
+    //    if (!suppressLogs) {
+    //        console.logger.info('Server started on ' + enums.options.hostname + ':' + enums.options.port);
+    //    }
+    //
+    //    // Configure the routes.
+        router(app, controllers);
+    
+    
+        // New referral code being registered by a click on a sharing button.
+        // Once referral code has been registered (PUT), it cannot be PUT again.
+        app.post('/r/:refCode', function(req, res) {
+            console.log('register refcode route')
+        
+            var refCode = req.params.refCode;
+        
+            if (referralPathFormat.test(req.path)) {
+                // Valid referral path.
+        
+                // See if the referral under the refcode is already persistently stored.
+                req.models.Referral.exists({
+                    refCode: refCode
+                }, function(err, exists) {
+                    if (!exists) {
+                        // No referral stored yet -- good!
+                
+                        var referral = {
+                            refCode: refCode,
+                            destinationPath: req.body.destinationPath,
+                            medium: req.body.medium,
+                            date: new Date(Date.now())
+                        };
+                
+                        req.models.Referral.create([referral], function (err, items) {
+                            if (err) {
+                                console.log(err);
+                            }
+                        
+                            console.log('created items');
+                            console.log(items);
+                        
+                            var createdReferral = items[0];
+                        
+                            console.log('createdReferral (1)');
+                            console.log(createdReferral);
+                        
+                            setUserOfInstance(createdReferral, req.user, function() {
+                            
+                                console.log('createdReferral (2)');
+                                console.log(createdReferral)
+                        
+                                createdReferral.save(function (err) {
+                                   if (err) {
+                                       console.log(err);
+                                   }
+                           
+                                   console.log('createdReferral (3)');
+                                   console.log(createdReferral);
+                           
+                                   res.status(201);
+                                   res.end();
+                           
+                                });
+                        
+                            });
+                    
+                        });
+                
+                    } else {
+                        // Referral code already exists
+                        res.status(204);
+                        res.end();
+                    }
+                });
+        
+            } else {
+                // Not valid referral path.
+                res.status(200);
+                res.end();
+            }
+        
+        });
+    
+
+    
+        http.createServer(app).listen(app.get('port'), function(){
+            console.log('Express server listening on port ' + app.get('port'));
+        });
+        
+    }
+
 
     //process.on('SIGINT', function () {
     //    console.logger.info('Server stopped.');
