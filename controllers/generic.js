3d4f0da19185ca096e67001f47e2cf777fb9093b
Luis Arenal Mijares <luis_arenal@hotmail.com>
2014-07-04 01:15:30 +0100

    Edit questions fix (again)

--- controllers/generic.js
+++ controllers/generic.js
@@ -333,7 +333,7 @@ exports.update = function (model, id, req, cb) {
                 if (postNew) {
                     // Update post.
                     req.models.Post.get(item.post_id, function (err, post) {
-                        var now = new Date().getTime();
+                        var now = new Date();
                         post.updated = now;
                         post.save(postNew, function (err) {
                             if (err) {
