4c450ae79c102d12858b6c203995f952f1cc706a
Enrico Costanza <ec@ecs.soton.ac.uk>
2015-04-23 08:32:59 +0100

    edited temp message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var on_http_request = function (req, res) {
     console.log('on_http_request');
     console.log('req.url', req.url);
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Veri.ly is temporary down for maintenance\n...sorry for the inconvenience.");
+    res.write("Veri.ly is temporarily down for maintenance\n..sorry for the inconvenience.\nIn the mean time you can contact us via contact@veri.ly");
     res.end();
     return;
 };
