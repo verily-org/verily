3e8a9b6d78d0108ad587f1fc90ee5921c268a063
Enrico Costanza <ec@ecs.soton.ac.uk>
2015-04-30 00:51:49 +0100

    fixed map position and marker

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,21 +1,33 @@
-var map_init = function(id) {
+var map_init = function(id, lat, lon) {
+    if (lat === undefined) {
+        lat = 0;
+    }
+    if (lon === undefined) {
+        lon = 0;
+    }
     var map = new OpenLayers.Map(id);
     layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
     map.addLayer(layer);
 
     map.setCenter(
-        new OpenLayers.LonLat(-1.422, 50.930).transform(
+        new OpenLayers.LonLat(lon, lat).transform(
             new OpenLayers.Projection("EPSG:4326"),
             map.getProjectionObject()
         ), 12
     );
 
     // Add Vector marker
+    var lonLat = new OpenLayers.LonLat(lon, lat)
+    .transform(
+        new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+        map.getProjectionObject() // to Spherical Mercator Projection
+    );
+    var point = new OpenLayers.Geometry.Point(lonLat.lon, lonLat.lat);
     var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
     var feature = new OpenLayers.Feature.Vector(
-        new OpenLayers.Geometry.Point(-71.147, 42.472),
+        point,
         {some:'data'},
-        {externalGraphic: 'img/marker.png', graphicHeight: 21, graphicWidth: 16});
+        {externalGraphic: 'https://01a8501412d0803e3903f1294229a3a0f79f839a.googledrive.com/host/0Bxs8G3uP6XL8Q3ZmQmQwNjRWNXM/location.png', graphicHeight: 21, graphicWidth: 16});
     vectorLayer.addFeatures(feature);
     map.addLayer(vectorLayer);
     return map;
